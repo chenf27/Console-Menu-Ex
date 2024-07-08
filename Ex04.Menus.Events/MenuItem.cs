97f759254455e2d19c732c0a944011c368f46db9
chenf27 <134041466+chenf27@users.noreply.github.com>
2024-07-08 21:43:56 +0300

    almost

--- Ex04.Menus.Events/MenuItem.cs
+++ Ex04.Menus.Events/MenuItem.cs
@@ -1,62 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Ex04.Menus.Events
 {
-    public delegate void MenuItemSelectedHandler(object sender, SelectEventArgs e);
-
-    public class SelectEventArgs : EventArgs
+    
+    public class MenuItemSelectedEventArgs : EventArgs
     {
-        private string m_Title;
-        private List<MenuItem> m_SubMenuItems;
+        public MenuItem SelectedMenuItem { get; }
 
-        public string Title { get { return m_Title; } }
-
-        public List<MenuItem> SubMenuItems { get { return m_SubMenuItems; } }
-
-        public SelectEventArgs(string i_Title, List<MenuItem> i_SubMenuItems)
+        public MenuItemSelectedEventArgs(MenuItem i_SelectedMenuItem)
         {
-            m_Title = i_Title;
-            m_SubMenuItems = i_SubMenuItems;
+            SelectedMenuItem = i_SelectedMenuItem;
         }
     }
+
+    public delegate void MenuItemSelectedEventHandler(object sender, MenuItemSelectedEventArgs e);
 
     public class MenuItem
     {
-        private string m_Title;
-        public event MenuItemSelectedHandler MenuItemSelectOccurred;
-        private List<MenuItem> m_SubMenuItems;
+        private readonly string r_Title;
+        private readonly MenuItem r_ParentNode;
+        private readonly bool r_IsMenu;
+        private readonly List<MenuItem> r_SubMenuItems;
+        private Action m_Method;
 
+        public event MenuItemSelectedEventHandler MenuItemSelectOccurred;
+
+        public MenuItem(MenuItem i_ParentNode, string i_Title, bool i_IsMenu)
+        {
+            r_ParentNode = i_ParentNode;
+            r_Title = i_Title;
+            r_IsMenu = i_IsMenu;
+
+            if (i_IsMenu)
+            {
+                r_SubMenuItems = new List<MenuItem>();
+            }
+        }
 
         public string Title
         {
-            get 
+            get
             {
-                return m_Title;
-            }
-            set
-            {
-                m_Title = value; 
+                return r_Title;
             }
         }
 
-        public void Selected()
+        public List<MenuItem> SubMenuItems
         {
-            SelectEventArgs e = new SelectEventArgs();
-            OnMenuItemSelectedOccurred(e);
+            get
+            {
+                //ERROR!
+                if (r_SubMenuItems == null)
+                {
+                    return new List<MenuItem>();
+                }
+                else
+                {
+                    return r_SubMenuItems;
+                }
+            }
         }
 
-        protected virtual void OnMenuItemSelectedOccurred(SelectEventArgs e)
+        public Action Method
+        {
+            get
+            {
+                return m_Method;
+            }
+        }
+
+        public bool IsMenu
+        {
+            get
+            {
+                return r_IsMenu;
+            }
+        }
+
+        public MenuItem ParentNode
+        {
+            get
+            {
+                return r_ParentNode;
+            }
+        }
+    
+        //TODO IN SET
+        public void SetAction(Action i_Method)
+        {
+            m_Method = i_Method;
+        }
+
+        public void AddSubMenuItem(MenuItem i_SubMenuItem)
+        {
+            //TODO EXEPTION HANDLE
+            if (r_IsMenu)
+            {
+                r_SubMenuItems.Add(i_SubMenuItem);
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot add sub-menu items to a non-menu item.");
+            }
+        }
+
+        public void MenuItemSelected()
+        {
+            OnMenuItemSelectOccurred(new MenuItemSelectedEventArgs(this));
+        }
+
+        protected virtual void OnMenuItemSelectOccurred(MenuItemSelectedEventArgs i_EventArguments)
         {
             if (MenuItemSelectOccurred != null)
             {
-                MenuItemSelectOccurred.Invoke(this, e);
+                MenuItemSelectOccurred.Invoke(this, i_EventArguments);
             }
         }
 
-
     }
+
 }
--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ex04.Menus.Interfaces;
+using Ex04.Menus.Events;
+
 
 namespace Ex04.Menus.Test
 {
@@ -12,10 +13,33 @@ namespace Ex04.Menus.Test
         public const bool v_IsMenu = true;
         public static void Main()
         {
-            Interfaces.MenuItem mainMenuItem = MenuItem(null, "Main Menu", v_IsMenu);
-            Interfaces.MainMenu mainMenu = new Interfaces.MainMenu(mainMenuItem);
+            Action showTimeAction = () => Console.WriteLine($"Current time: {DateTime.Now}");
+            Action showDateAction = () => Console.WriteLine($"Current date: {DateTime.Now.ToShortDateString()}");
 
-            mainMenu.Show();
+            // Create menu items
+            MenuItem mainMenu = new MenuItem(null, "Main Menu", true);
+            MenuItem subMenu1 = new MenuItem(mainMenu, "Sub Menu 1", true);
+            MenuItem subMenu2 = new MenuItem(mainMenu, "Sub Menu 2", true);
+            MenuItem actionItem1 = new MenuItem(subMenu1, "Show Time", false);
+            actionItem1.SetAction(showTimeAction);
+            MenuItem actionItem2 = new MenuItem(subMenu2, "Show Date", false);
+            actionItem2.SetAction(showDateAction);
+
+            // Add sub menu items
+            subMenu1.AddSubMenuItem(actionItem1);
+            subMenu2.AddSubMenuItem(actionItem2);
+            mainMenu.AddSubMenuItem(subMenu1);
+            mainMenu.AddSubMenuItem(subMenu2);
+
+            // Create main menu instance
+            MainMenu menu = new MainMenu("Main Menu");
+            menu.AddMenuItem(menu.Main, subMenu1);
+            menu.AddMenuItem(menu.Main, subMenu2);
+            menu.AddMenuItem(subMenu1, actionItem1);
+            menu.AddMenuItem(subMenu2, actionItem2);
+
+            // Display the menu
+            menu.Show();
         }
     }
 }
