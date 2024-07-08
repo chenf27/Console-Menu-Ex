f2c3f9eb3920ce9170a3afa61c0edbf8fc1fec42
Yuval <yuvalla@mta.ac.il>
2024-07-08 17:32:23 +0300

    start of interface project

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu : ISelectedItem
+    {
+        private Menu m_MainMenu;
+        
+        public void AddMenuItem()
+        {
+            string title = "";
+            MenuItem newItem = new MenuItem(title);
+            
+            newItem.AddListener(this);
+        }
+
+        void ISelectedItem.Selected(MenuItem i_Item)
+        {
+            /// TODO implement
+        }
+
+        public void Show()
+        {
+            Console.Clear();
+            m_MainMenu.Show();
+        }
+
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuItem
+    {
+        private readonly string m_Title;
+        private List<ISelectedItem> m_SelectedListeners;
+        
+        public MenuItem(string i_Title)
+        {
+            m_Title = i_Title;
+        }
+        
+        public string Title
+        { 
+            get 
+            { 
+                return m_Title; 
+            }
+        }
+
+        public void AddListener(ISelectedItem i_MenuItem)
+        {
+            if (m_SelectedListeners == null)
+            {
+                m_SelectedListeners = new List<ISelectedItem>();
+            }
+
+            m_SelectedListeners.Add(i_MenuItem);
+        }
+
+        public void RemoveListener(ISelectedItem i_MenuItem)
+        {
+            m_SelectedListeners.Remove(i_MenuItem);
+        }
+     
+        private void notifyAll()
+        {
+            foreach(ISelectedItem selectedItem in m_SelectedListeners)
+            {
+                selectedItem.Selected(this);
+            }
+        }
+    }
+}
