1091c395a1eb583af9bc002ba898f2b40568ac23
chenf27 <134041466+chenf27@users.noreply.github.com>
2024-07-08 15:48:59 +0300

    tmp

---
+++ Ex04.Menus.Events/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Events
+{
+    public class MainMenu
+    {
+        private List<MenuItem> r_MenuItems;
+
+        public MainMenu()
+        {
+            r_MenuItems = new List<MenuItem>();
+        }
+
+        public void AddMenuItem(MenuItem menuItem)
+        {
+            r_MenuItems.Add(menuItem);
+        }
+
+
+    }
+}
---
+++ Ex04.Menus.Events/MenuItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Events
+{
+    public delegate void MenuItemSelectedHandler(object sender, SelectEventArgs e);
+
+    public class SelectEventArgs : EventArgs
+    {
+        private string m_Title;
+        private List<MenuItem> m_SubMenuItems;
+
+        public string Title { get { return m_Title; } }
+
+        public List<MenuItem> SubMenuItems { get { return m_SubMenuItems; } }
+
+        public SelectEventArgs(string i_Title, List<MenuItem> i_SubMenuItems)
+        {
+            m_Title = i_Title;
+            m_SubMenuItems = i_SubMenuItems;
+        }
+    }
+
+    public class MenuItem
+    {
+        private string m_Title;
+        public event MenuItemSelectedHandler MenuItemSelectOccurred;
+        private List<MenuItem> m_SubMenuItems;
+
+
+        public string Title
+        {
+            get 
+            {
+                return m_Title;
+            }
+            set
+            {
+                m_Title = value; 
+            }
+        }
+
+        public void Selected()
+        {
+            SelectEventArgs e = new SelectEventArgs();
+            OnMenuItemSelectedOccurred(e);
+        }
+
+        protected virtual void OnMenuItemSelectedOccurred(SelectEventArgs e)
+        {
+            if (MenuItemSelectOccurred != null)
+            {
+                MenuItemSelectOccurred.Invoke(this, e);
+            }
+        }
+
+
+    }
+}
